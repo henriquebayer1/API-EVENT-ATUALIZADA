37c6bdcb225504c37f83eff19406b8b4f6225a1c
Henrique Jordão Baier De Azevedo <45922978810@info.local>
2023-12-01 17:08:00 -0300

    .

---
+++ Controllers/PresencasEventoController.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using webapi.event_.Domains;
+using webapi.event_.Interfaces;
+using webapi.event_.Repositories;
+
+namespace webapi.event_.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Produces("Application/json")]
+    public class PresencasEventoController : ControllerBase
+    {
+        private IPresencasEventoRepository _presencasEventoRepository { get; set; }
+
+        public PresencasEventoController()
+        {
+            _presencasEventoRepository = new PresencaRepository();
+        }
+
+
+        [HttpGet]
+        public IActionResult Get()
+
+        {
+            try
+            {
+                return Ok(_presencasEventoRepository.Listar());
+
+
+            }
+            catch (Exception e)
+            {
+
+                return BadRequest(e.Message);
+            }
+
+
+
+        }
+
+
+        [HttpPost]
+        public IActionResult Post(PresencasEvento presencasEvento)
+
+        {
+            try
+            {
+                _presencasEventoRepository.Inscrever(presencasEvento);
+
+                return StatusCode(202);
+            }
+            catch (Exception e)
+            {
+
+                return BadRequest(e.Message);
+            }
+
+
+
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(Guid id)
+
+        {
+            try
+            {
+                _presencasEventoRepository.Deletar(id);
+
+                return StatusCode(202);
+            }
+            catch (Exception e)
+            {
+
+                return BadRequest(e.Message);
+            }
+
+
+
+
+        }
+
+
+    }
+}
